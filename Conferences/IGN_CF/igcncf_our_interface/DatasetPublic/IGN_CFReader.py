56da30c7e7d53e2c404e6f6fd57185b99a6dfd37
m3f1570 <gianmarco.lodari@mail.polimi.it>
2022-10-28 18:16:00 +0200

    Changes by francesca

---
+++ Conferences/IGN_CF/igcncf_our_interface/DatasetPublic/IGN_CFReader.py
@@ -0,0 +1,20 @@
+import torch
+from Conferences.IGN_CF.igcncf_github.utils import init_run
+
+from dataset import get_dataset
+from Conferences.IGN_CF.igcncf_github.config import get_gowalla_config, get_yelp_config, get_amazon_config
+from tensorboardX import SummaryWriter
+
+def init_file_and_device() :
+
+        log_path = __file__[:-3]
+        init_run(log_path, 2021)
+        device = torch.device('cpu')
+        return device, log_path
+
+def acquire_dataset(log_path,config):
+    dataset_config, model_config, trainer_config = config[2]
+    dataset_config['path'] = dataset_config['path'][:-4] + str(1)
+    writer = SummaryWriter(log_path)
+    dataset = get_dataset(dataset_config)
+    return dataset
