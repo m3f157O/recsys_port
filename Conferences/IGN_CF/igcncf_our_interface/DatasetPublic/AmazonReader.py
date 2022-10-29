b31515e0848f1825fe747a8ca3e94853920a1ed7
francesca.forbicini@mail.polimi.it <francesca.forbicini@mail.polimi.it>
2022-10-29 20:15:00 +0200

    Updated AmazonReader.py

--- Conferences/IGN_CF/igcncf_our_interface/DatasetPublic/AmazonReader.py
+++ Conferences/IGN_CF/igcncf_our_interface/DatasetPublic/AmazonReader.py
@@ -5,14 +5,21 @@ Created on 08/11/18
 
 @author: Maurizio Ferrari Dacrema
 """
-import numpy as np
 
-from Recommenders.DataIO import DataIO
 from Data_manager.split_functions.split_train_validation_random_holdout import \
     split_train_in_two_percentage_global_sample
+
+from Recommenders.DataIO import DataIO
+
+from IGN_CFReader import *
+
+from Conferences.IGN_CF.igcncf_github.config import get_gowalla_config, get_yelp_config, get_amazon_config
+
+import numpy as np
 import scipy.sparse as sp
+
 import os
-from Data_manager.AmazonReviewData.AmazonBooksReader import AmazonBooksReader as AmazonBookReader_DataManager
+from Data_manager.AmazonReviewData.AmazonBooksReader import AmazonBooksReader as AmazonReader_DataManager
 
 
 class AmazonReader(object):
@@ -26,12 +33,19 @@ class AmazonReader(object):
         pre_splitted_path += "data_split/"
         pre_splitted_filename = "splitted_data_"
 
+        dataIO = DataIO(pre_splitted_path)
+
         # If directory does not exist, create
         if not os.path.exists(pre_splitted_path):
             os.makedirs(pre_splitted_path)
 
-        dataIO = DataIO(pre_splitted_path)
+        device, log_path = init_file_and_device()
 
+        config = get_gowalla_config(device)
+
+        dataset = acquire_dataset(log_path, config)
+
+        ##TODO PUT ^ IN EXCEPTION, CORRECTLY DOWNLOAD DATA IF NOT THERE ;) (OvO)
         try:
 
             print("AmazonReader: Attempting to load pre-splitted data")
@@ -46,18 +60,18 @@ class AmazonReader(object):
 
             print("AmazonReader: loading URM")
 
-            # Done Replace this with the publicly available dataset you need
+            # DO Replace this with the publicly available dataset you need
             #  The DataManagers are in the Data_Manager folder, if the dataset is already there use that data reader
-            data_reader = AmazonBookReader_DataManager()
+            data_reader = AmazonReader_DataManager()
             dataset = data_reader.load_data()
 
             URM_all = dataset.get_URM_all()
 
-
-            # TODO MUST BE CHECKED Apply data preprocessing if required (for example binarizing the data, removing users ...)
-            # binarize the data (only keep ratings >= 3) and users and items must have more than 10 interactions
-            URM_all.data = URM_all.data >= 3.0
+            # TODO Apply data preprocessing if required (for example binarizing the data, removing users ...)
+            # binarize the data (only keep ratings >= 4)
+            URM_all.data = URM_all.data >= 4.0
             URM_all.eliminate_zeros()
+            # TODO MUST BE CHECKED Apply data preprocessing if required (for example binarizing the data, removing users ...)
             for user_id in range(np.shape(URM_all)[0]):
                 start_pos = URM_all.tocsr.indptr[user_id]
                 end_pos = URM_all.tocsr.indptr[user_id + 1]
@@ -71,11 +85,11 @@ class AmazonReader(object):
                     URM_all.tocsc.indices[start_pos:end_pos] = 0
             URM_all.eliminate_zeros()
 
-            # Done select the data splitting that you need, almost certainly there already is a function that does the splitting
+            # TODO select the data splitting that you need, almost certainly there already is a function that does the splitting
             #  in the way you need, if you are not sure, ask me via email
-            # Split the data in train 70, validation 10 and test 20
-            URM_train, URM_test = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.7)
-            URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_train, train_percentage=0.9)
+            # Split the data in train, validation and test
+            URM_train, URM_test = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.8)
+            URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_train, train_percentage=0.8)
 
             # Done get the sparse matrices in the correct dictionary with the correct name
             # TODO ICM_DICT and UCM_DICT can be empty if no ICMs or UCMs are required
@@ -87,7 +101,7 @@ class AmazonReader(object):
             self.UCM_DICT = {}
 
             self.URM_DICT = {
-                "URM_train": URM_train,
+                "URM_train": dataset.get_URM_all(),
                 "URM_test": URM_test,
                 "URM_validation": URM_validation,
             }
@@ -101,4 +115,4 @@ class AmazonReader(object):
 
             dataIO.save_data(pre_splitted_filename, data_dict_to_save=data_dict_to_save)
 
-            print("AmazonBookReader: loading complete")
+            print("AmazonReader: loading complete")
